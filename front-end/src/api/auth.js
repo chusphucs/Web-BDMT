23e2c564e16be2d38158d1aaa4272127584591c8
HoThiHieu <92620426+HoThiHieu@users.noreply.github.com>
2022-12-01 22:49:12 +0700

    [Front end] Gọi API hiển thị danh sách post, thêm post

--- front-end/src/api/auth.js
+++ front-end/src/api/auth.js
@@ -2,15 +2,15 @@ import axiosClient from './axiosClient'
 
 const auth = {
     login: (credentials) => {
-        const url = 'api/auth/login'
+        const url = '/api/auth/login'
         return axiosClient.post(url, credentials)
     },
     register: (credentials) => {
-        const url = 'api/auth/register'
+        const url = '/api/auth/register'
         return axiosClient.post(url, credentials)
     },
     getAuthenticatedUser: () => {
-        const url = 'api/auth/get-authenticated-user'
+        const url = '/api/auth/get-authenticated-user'
         return axiosClient.get(url)
     },
 }
--- front-end/src/api/uploadImageApi.js
+++ front-end/src/api/uploadImageApi.js
@@ -2,7 +2,7 @@ import axiosClient from './axiosClient'
 
 const uploadImageApi = {
     uploadUserAvatar: (userId, image) => {
-        const url = `api/upload/avatar/user/${userId}`
+        const url = `/api/upload/avatar/user/${userId}`
         return axiosClient.post(url, image)
     },
 }
